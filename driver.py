4a5dd9d5430220c26ae0bb78fb0a8d36f0e33016
tkoar <terrancekoar@gmail.com>
2018-05-09 12:42:46 -0400

    removed solution

--- driver.py
+++ driver.py
@@ -1,26 +1,12 @@
 # remeber to import the trip class here
 
-from trip import Trip
 class Driver:
 
     def __init__(self, name):
         self.name = name
 
-    def name():
-        doc = "The name property."
-        def fget(self):
-            return self._name
-        def fset(self, value):
-            self._name = value
-        def fdel(self):
-            del self._name
-        return locals()
-    name = property(**name())
-
     def my_trips(self):
-        trips = Trip.all()
-        return [trip for trip in trips if trip.driver == self]
+        pass
 
     def my_trip_summaries(self):
-        trips = self.my_trips()
-        return ["{} to {}".format(trip.start, trip.destination) for trip in trips]
+        pass
--- owner.py
+++ owner.py
@@ -1,5 +1,4 @@
 # import car class here
-from car import Car
 
 class Owner:
 
@@ -7,28 +6,5 @@ class Owner:
         self._name = name
         self._age = age
 
-    def name():
-        doc = "The name property."
-        def fget(self):
-            return self._name
-        def fset(self, value):
-            self._name = value
-        def fdel(self):
-            del self._name
-        return locals()
-    name = property(**name())
-
-    def age():
-        doc = "The age property."
-        def fget(self):
-            return self._age
-        def fset(self, value):
-            self._age = value
-        def fdel(self):
-            del self._age
-        return locals()
-    age = property(**age())
-
     def find_my_cars(self):
-        my_cars = [car for car in Car.all() if car.owner == self]
-        return [f"{car.make} {car.model}" for car in my_cars]
+        pass
